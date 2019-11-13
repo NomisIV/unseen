a3cb0a83040a1b7beb80ffbb4c66aad649aa2a05
NomisIV <simon.gutgesell@gmail.com>
2019-11-13 20:04:44 +0100

    Added eslint-config-prettier
    I literally just sold this project to the devil

--- scripts/game.js
+++ scripts/game.js
@@ -48,43 +48,43 @@ function setup() {
 
     // Init game objects
     switch (screen) {
-    case "start":
-        break;
-    case "game":
-        p = new Player(50, 50, 0);
-        // Add sound at [500, 500] with frequency 200Hz with 500ms beeps.
-        sounds.push(new Thing(500, 500, 200, 500));
-        break;
+        case "start":
+            break;
+        case "game":
+            p = new Player(50, 50, 0);
+            // Add sound at [500, 500] with frequency 200Hz with 500ms beeps.
+            sounds.push(new Thing(500, 500, 200, 500));
+            break;
     }
 }
 
 /* ======== GAME LOOP ======== */
 function draw() {
     switch (screen) {
-    case "start":
-        background(255, 0, 0);
+        case "start":
+            background(255, 0, 0);
 
-        break;
-    case "game":
-        background(0);
+            break;
+        case "game":
+            background(0);
 
-        // Movement
-        if (keys.w) p.move(Math.sin(p.a) * step, Math.cos(p.a) * step, 0);
-        if (keys.s) p.move(-Math.sin(p.a) * step, -Math.cos(p.a) * step, 0);
-        if (keys.a) p.a -= PI / 64;
-        if (keys.d) p.a += PI / 64;
-        p.a = restrictAngle(p.a);
-        // p.a = ((p.a + PI) % 2) - PI;
+            // Movement
+            if (keys.w) p.move(Math.sin(p.a) * step, Math.cos(p.a) * step, 0);
+            if (keys.s) p.move(-Math.sin(p.a) * step, -Math.cos(p.a) * step, 0);
+            if (keys.a) p.a -= PI / 64;
+            if (keys.d) p.a += PI / 64;
+            p.a = restrictAngle(p.a);
+            // p.a = ((p.a + PI) % 2) - PI;
 
-        // Draw objects
-        p.draw();
+            // Draw objects
+            p.draw();
 
-        // Play sounds
-        sounds.forEach(s => {
-            s.draw();
-            s.play();
-        });
-        break;
+            // Play sounds
+            sounds.forEach(s => {
+                s.draw();
+                s.play();
+            });
+            break;
     }
 }
 
